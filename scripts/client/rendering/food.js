d27f9d4513a1c1f643b08d61be011c50a97b5046
Ann Marie Humble <A02324311@aggies.usu.edu>
2024-04-15 20:59:54 -0600

    only renders food within the render distance

--- scripts/client/rendering/food.js
+++ scripts/client/rendering/food.js
@@ -10,6 +10,7 @@ MyGame.renderer.Food = (function(graphics) {
     
     const LEFT_WALL_X = 0;
     const TOP_WALL_Y = 0;
+    const SCREEN_WIDTH = 1;
     
     // ------------------------------------------------------------------
     //
@@ -18,34 +19,33 @@ MyGame.renderer.Food = (function(graphics) {
     // ------------------------------------------------------------------
     that.render = function(model, texture, playerSelfPos, WORLD_SIZE) {
         let screenPos = {x: playerSelfPos.x - .5, y: playerSelfPos.y - .5}; //top-left corner of screen
-        console.log("screenPos: ", screenPos);
-        // console.log(-screenPos.y);
 
+        graphics.saveContext();
         for (let i = 0; i < model.positionsX.length; i++) {
-            graphics.saveContext();
-
-            // render food if in render dist
-            // if (screenPos.x < LEFT_WALL_X && ) {
-                let foodX = model.positionsX[i] - screenPos.x;
-                let foodY = model.positionsY[i] - screenPos.y;
-                let position = {
-                    x: foodX,
-                    y: foodY,
-                }
-                // console.log("position: ", position);
-                // PROLLY CHANGE THIS vvv TO JUST A SEPARATE FOOD THING FOR BIG FOOD
-                // if (model.bigFood[i]) { // this isn't working; also we need to get bigFood values from server cause those should be the same across clients!!
-                //     graphics.drawSprite(texture[model.spriteSheetIndices[i]], position, {width: model.size.width * 2, height: model.size.height * 2}, model.renderFrame); 
-                // }
-                // else {
-                    graphics.drawSprite(texture[model.spriteSheetIndices[i]], position, model.size, model.renderFrame); 
-                // }
     
-            // }
-            
-            // we need the last i in this call to increment every time the function is called; that way we render a new frame of the frog each time
-            graphics.restoreContext();
+            // render food if in render dist
+            if (screenPos.x < model.positionsX[i] && model.positionsX[i] < screenPos.x + SCREEN_WIDTH) {
+                if (screenPos.y < model.positionsY[i] && model.positionsY[i] < screenPos.y + SCREEN_WIDTH) {
+                    console.log("drawing frog ", i);
+                    let foodX = model.positionsX[i] - screenPos.x;
+                    let foodY = model.positionsY[i] - screenPos.y;
+                    let position = {
+                        x: foodX,
+                        y: foodY,
+                    }
+                    // console.log("position: ", position);
+                    // PROLLY CHANGE THIS vvv TO JUST A SEPARATE FOOD THING FOR BIG FOOD
+                    // if (model.bigFood[i]) { // this isn't working; also we need to get bigFood values from server cause those should be the same across clients!!
+                    //     graphics.drawSprite(texture[model.spriteSheetIndices[i]], position, {width: model.size.width * 2, height: model.size.height * 2}, model.renderFrame); 
+                    // }
+                    // else {
+                        graphics.drawSprite(texture[model.spriteSheetIndices[i]], position, model.size, model.renderFrame); 
+                }                        
+            }
         }
+            
+        // we need the last i in this call to increment every time the function is called; that way we render a new frame of the frog each time
+        graphics.restoreContext();
     }
 
     return that;
