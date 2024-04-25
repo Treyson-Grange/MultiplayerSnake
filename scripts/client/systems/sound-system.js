83d7040a8c14a8667220dfa07e4459f3f8e5f87c
Treyson Grange <treyson.grange@outlook.com>
2024-04-24 21:30:56 -0600

    cropped gulp audio

--- scripts/client/systems/sound-system.js
+++ scripts/client/systems/sound-system.js
@@ -3,55 +3,54 @@
 // This function performs the one-time game initialization.
 //
 //------------------------------------------------------------------
-MyGame.systems.SoundSystem = (function(systems) {
-    'use strict';
+MyGame.systems.SoundSystem = (function (systems) {
+  "use strict";
 
-    function loadSound(source) {
-        let sound = new Audio();
-        // sound.addEventListener('canplay', function() {
-        //     // console.log(`${source} is ready to play`);
-        // });
-        sound.addEventListener('play', function() {
-        });
-        // sound.addEventListener('pause', function() {
-        //     // console.log(`${source} paused`);
-        // });
-        // sound.addEventListener('canplaythrough', function() {
-        //     // console.log(`${source} can play through`);
-        // });
-        // sound.addEventListener('progress', function() {
-        //     // console.log(`${source} progress in loading`);
-        // });
-        // sound.addEventListener('timeupdate', function() {
-        //     // console.log(`${source} time update: ${this.currentTime}`);
-        // });
-        sound.src = source;
-        return sound;
-    }
+  function loadSound(source) {
+    let sound = new Audio();
+    // sound.addEventListener('canplay', function() {
+    //     // console.log(`${source} is ready to play`);
+    // });
+    sound.addEventListener("play", function () {});
+    // sound.addEventListener('pause', function() {
+    //     // console.log(`${source} paused`);
+    // });
+    // sound.addEventListener('canplaythrough', function() {
+    //     // console.log(`${source} can play through`);
+    // });
+    // sound.addEventListener('progress', function() {
+    //     // console.log(`${source} progress in loading`);
+    // });
+    // sound.addEventListener('timeupdate', function() {
+    //     // console.log(`${source} time update: ${this.currentTime}`);
+    // });
+    sound.src = source;
+    return sound;
+  }
 
-    function loadAudio() {
-        MyGame.sounds = {}
-        MyGame.sounds['gulp'] = loadSound('assets/audio/treyson-gulping.m4a');
-        MyGame.sounds['end-game'] = loadSound('assets/audio/Lily-end-game.m4a');
-    }
+  function loadAudio() {
+    MyGame.sounds = {};
+    MyGame.sounds["gulp"] = loadSound("assets/audio/treyson-gulping.mp3");
+    MyGame.sounds["end-game"] = loadSound("assets/audio/Lily-end-game.m4a");
+  }
 
-    console.log('initializing...');
+  console.log("initializing...");
 
-    loadAudio();
+  loadAudio();
 
-    //------------------------------------------------------------------
-    //
-    // Plays the specified audio
-    //
-    //------------------------------------------------------------------
-    function playSound(whichSound) {
-        console.log("playing: ", whichSound);
-        MyGame.sounds[whichSound].play();
-    }
+  //------------------------------------------------------------------
+  //
+  // Plays the specified audio
+  //
+  //------------------------------------------------------------------
+  function playSound(whichSound) {
+    console.log("playing: ", whichSound);
+    MyGame.sounds[whichSound].play();
+  }
 
-    let api = {
-        playSound: playSound
-    };
+  let api = {
+    playSound: playSound,
+  };
 
-    return api;
-}(MyGame.systems));
+  return api;
+})(MyGame.systems);
